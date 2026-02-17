6bbd2767401a10b8353ed8a79dfb411575f996dd
Leo <leograyc@duck.com>
2026-02-17 04:01:38 -0600

    debugging ik

---
+++ training/utils/prepare_specialized_urdf.py
@@ -0,0 +1,249 @@
+#!/usr/bin/env python3
+"""
+This code was taken and adapted from 'stretch_dex_teleop' (Hello Robot Inc.).
+
+GOAL: Generate an 8-DOF "Omni-Mobile" URDF for the Stretch 3. 
+By nesting the virtual 'joint_mobile_base_translation' joint as a child of the 
+'joint_mobile_base_rotation' joint, we resolve the non-holonomic constraint 
+mathematically. This ensures the robot's forward translation axis always 
+aligns with its current base rotation, allowing the IK solver to command 
+fluid whole-body mobile manipulation for the Universal Kitchen Assistant.
+"""
+import pathlib
+from urdf_parser_py import urdf as ud
+import numpy as np
+import pprint
+import os
+import sys
+from copy import deepcopy
+
+# This adds the directory containing dex_teleop_parameters.py to your search path
+SCRIPT_DIR = os.path.dirname(os.path.abspath(__file__))
+DEX_TELEOP_DIR = os.path.join(SCRIPT_DIR, '..', '..', 'dependencies', 'stretch_dex_teleop')
+sys.path.insert(0, DEX_TELEOP_DIR)
+
+import dex_teleop_parameters as dt
+def save_urdf_file(robot, file_name):
+    urdf_string = robot.to_xml_string()
+    print('Saving new URDF file to', file_name)
+    fid = open(file_name, 'w')
+    fid.write(urdf_string)
+    fid.close()
+    print('Finished saving')
+
+
+use_on_robot = True
+
+if use_on_robot:
+    import stretch_body.hello_utils as hu
+    calibration_dir = pathlib.Path(hu.get_fleet_directory()) / 'exported_urdf'
+    urdf_path = calibration_dir / 'stretch.urdf'
+    controller_params_path = calibration_dir / 'controller_calibration_head.yaml'
+    urdf_filename = str(urdf_path.absolute())
+else:
+    urdf_filename = "./stretch.urdf"
+
+
+non_fixed_joints = [
+    'joint_mobile_base_translation', # Added for 8-DOF
+    'joint_mobile_base_rotation',    # Added for 8-DOF
+    'joint_lift',
+    'joint_arm_l0',
+    'joint_arm_l1',
+    'joint_arm_l2',
+    'joint_arm_l3',
+    'joint_wrist_yaw',
+    'joint_wrist_pitch',
+    'joint_wrist_roll',
+]
+
+
+# Enables more conservative joint limits to be set than in the
+# original URDF. Joint limits that are outside the originally
+# permitted range will be clipped to the original range. Joint limits
+# with a value of None will be set to the original limit.
+
+use_original_limits = True #False
+
+if use_original_limits:
+    # Beware of gimbal lock if joint_wrist_pitch is too close to -90 deg
+    
+    ik_joint_limits = {
+        'joint_mobile_base_translation' : (None, None), # Added
+        'joint_mobile_base_rotation' : (None, None),    # Added
+        'joint_lift' : (None, None),
+        'joint_arm_l0' : (None, None),
+        'joint_wrist_yaw' : (None, None),
+        'joint_wrist_pitch' : (dt.wrist_pitch_lower_limit, None), 
+        'joint_wrist_roll' : (None, None)
+    }
+
+    #'joint_wrist_pitch' : (None, None), # default lowest pitch is -1.57 rad on the default URDF (-89.954373836 deg)~
+
+else:
+    
+    ik_joint_limits = {
+        'joint_mobile_base_translation' : (-0.25, 0.25),
+        'joint_mobile_base_rotation' : ( -(np.pi/2.0), np.pi/2.0 ),
+        'joint_lift' : (0.01, 1.09),
+        'joint_arm_l0' : (0.01, 0.48),
+        'joint_wrist_yaw' : ( -(np.pi/4.0), np.pi ),
+        'joint_wrist_pitch' : ( -0.9 * (np.pi/2.0), np.pi/20.0 ),
+        'joint_wrist_roll' : ( -(np.pi/2.0), np.pi/2.0 )
+        }
+
+    print('Setting new joint limits with ik_joint_limits =')
+    pprint.pprint(ik_joint_limits)
+
+    
+# Load and store the original uncalibrated URDF.
+print()
+print('Loading URDF from:')
+print(urdf_filename)
+print('The specialized URDFs will be derived from this URDF.')
+robot = ud.Robot.from_xml_file(urdf_filename)
+
+# Change any joint that should be immobile for end effector IK into a fixed joint
+for j in robot.joint_map.keys():
+    if j not in non_fixed_joints:
+        joint = robot.joint_map[j]
+        #print('(joint name, joint type) =', (joint.name, joint.type))
+        joint.type = 'fixed'
+        
+# Replace telescoping arm with a single prismatic joint
+
+# arm joints from proximal to distal
+all_arm_joints = [
+    'joint_arm_l4',
+    'joint_arm_l3',
+    'joint_arm_l2',
+    'joint_arm_l1',
+    'joint_arm_l0'
+]
+
+prismatic_arm_joints = all_arm_joints[1:]
+
+removed_arm_joints = all_arm_joints[1:-1]
+
+xyz_total = np.array([0.0, 0.0, 0.0])
+limit_upper_total = 0.0
+
+for j in prismatic_arm_joints:
+    joint = robot.joint_map[j]
+    #print(j + ' =', joint)
+    xyz = joint.origin.xyz
+    #print('xyz =', xyz)
+    xyz_total = xyz_total + xyz
+    limit_upper = joint.limit.upper
+    #print('limit_upper =', limit_upper)
+    limit_upper_total = limit_upper_total + limit_upper
+    
+#print('xyz_total =', xyz_total)
+#print('limit_upper_total =', limit_upper_total)
+
+proximal_arm_joint = robot.joint_map[all_arm_joints[0]]
+near_proximal_arm_joint = robot.joint_map[all_arm_joints[1]]
+near_distal_arm_joint = robot.joint_map[all_arm_joints[-2]]
+distal_arm_joint = robot.joint_map[all_arm_joints[-1]]
+
+# Directly connect the proximal and distal parts of the arm
+distal_arm_joint.parent = near_proximal_arm_joint.parent
+
+# Make the distal prismatic joint act like the full arm
+distal_arm_joint.origin.xyz = xyz_total
+distal_arm_joint.limit.upper = limit_upper_total
+
+# Make the telescoping joints in between immobile
+for j in removed_arm_joints:
+    joint = robot.joint_map[j]
+    joint.type = 'fixed'
+
+
+robot_rotary = robot
+robot_prismatic = deepcopy(robot)
+
+###############################################
+# CREATE 8-DOF NESTED OMNI-MOBILE BASE
+# Chain: virtual_base -> rotation_link -> base_link
+robot_omni = deepcopy(robot)
+
+# 1. Add intermediate links/virtual base
+robot_omni.add_link(ud.Link(name='virtual_base'))
+robot_omni.add_link(ud.Link(name='base_rotation_link'))
+
+# 2. Add ROTATION Joint (World -> Rotation Link)
+robot_omni.add_joint(ud.Joint(
+    name='joint_mobile_base_rotation',
+    parent='virtual_base',
+    child='base_rotation_link',
+    joint_type='revolute',
+    axis=[0, 0, 1],
+    origin=ud.Pose(xyz=[0, 0, 0], rpy=[0, 0, 0]),
+    limit=ud.JointLimit(effort=10, velocity=1, lower=-np.pi, upper=np.pi)
+))
+
+# 3. Add TRANSLATION Joint (Rotation Link -> Base Link)
+# Nesting this here ensures +X is always "Forward" relative to current rotation
+robot_omni.add_joint(ud.Joint(
+    name='joint_mobile_base_translation',
+    parent='base_rotation_link',
+    child='base_link',
+    joint_type='prismatic',
+    axis=[1, 0, 0],
+    origin=ud.Pose(xyz=[0, 0, 0], rpy=[0, 0, 0]),
+    limit=ud.JointLimit(effort=10, velocity=1, lower=-1.0, upper=1.0)
+))
+
+###############################################
+
+# When specified, this sets more conservative joint limits than the
+# original URDF. Joint limits that are outside the originally
+# permitted range are clipped to the original range. Joint limits
+# with a value of None are set to the original limit.
+for robot in [robot_rotary, robot_prismatic]: 
+    for j in ik_joint_limits:
+        joint = robot.joint_map.get(j, None)
+        if joint is not None: 
+
+            original_upper = joint.limit.upper
+            requested_upper = ik_joint_limits[j][1]
+            #print()
+            #print('joint =', j)
+            #print('original_upper =', original_upper)
+            #print('requested_upper =', requested_upper)
+            if requested_upper is not None:
+                new_upper = min(requested_upper, original_upper)
+                #print('new_upper =', new_upper)
+                robot.joint_map[j].limit.upper = new_upper
+                #print()
+
+
+            original_lower = joint.limit.lower
+            requested_lower = ik_joint_limits[j][0]
+            if requested_lower is not None:
+                new_lower = max(requested_lower, original_lower)
+                robot.joint_map[j].limit.lower = new_lower
+        
+
+#print('************************************************')
+#print('after adding link and joint: robot =', robot)
+#print('************************************************')
+
+print()
+save_urdf_file(robot_omni, 'stretch_omni_mobile_ik.urdf')
+
+# Create version with fixed wrist
+print('Prepare 8-DOF URDF with a fixed wrist.')
+robot_fixed_wrist = deepcopy(robot_omni)
+non_fixed_base_joints = [
+    'joint_mobile_base_translation',
+    'joint_mobile_base_rotation',
+    'joint_lift',
+    'joint_arm_l0'
+]
+
+for j in robot_fixed_wrist.joint_map.keys():
+    if j not in non_fixed_base_joints:
+        robot_fixed_wrist.joint_map[j].type = 'fixed'
+            
+save_urdf_file(robot_fixed_wrist, 'stretch_omni_mobile_ik_with_fixed_wrist.urdf')
